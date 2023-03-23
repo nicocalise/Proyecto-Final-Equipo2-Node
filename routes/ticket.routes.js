7c48906bb8d1e79436ea8016a974ea3b9a358500
nicolascalise <nicolas.calise@bootcamp-upgrade.com>
2023-03-23 17:45:40 +0100

    rebase

---
+++ routes/ticket.routes.js
@@ -0,0 +1,78 @@
+const express = require('express');
+const Ticket = require('../models/Ticket');
+
+const router = express.Router();
+
+//Find all
+router.get('/', async (req, res, next) =>{
+    try{
+        const tickets = await Ticket.find().populate('event');
+        return res.status(200).json(tickets);
+    }catch(err){
+        return next(err);
+    }
+});
+
+////find by id
+//router.get('/:id', async(req, res, next)=>{
+//    const { id } = req.params;
+//    try{
+//        const recipe = await Recipe.findById(id).populate('ingredients.ingredient');
+//        return res.status(200).json(recipe);
+//    }catch(err){
+//        return next(err);
+//    }
+//});
+
+////post
+//router.post('/', async(req, res, next)=>{
+//    try{
+//        const recipe = new Recipe({
+//            name: req.body.name,
+//            image: recipePicture,
+//            ingredients : []
+//        });
+//        const createdRecipe = await recipe.save();
+//        res.status(201).json(createdRecipe);
+//    }catch(err){
+//        return next(err);
+//    }
+//});
+//
+////add ingredient to a recipe
+//router.put('/:recipeId/add-ingredient', async (req, res, next) => {
+//    try {
+//        const { recipeId } = req.params;
+//        const { ingredientId } = req.body;
+//        const { quantity} = req.body;
+//        const updatedRecipe = await Recipe.findByIdAndUpdate(
+//            recipeId,
+//            { $push:{ 
+//                        ingredients:{ 
+//                                        ingredient:ingredientId,
+//                                        quantity:quantity
+//                                    }   
+//                    }
+//            },
+//            { new: true }
+//        );
+//        return res.status(200).json(updatedRecipe);
+//    } catch (error) {
+//        return next(error);
+//    }
+//});
+//
+////delete
+//router.delete('/:id', async (req, res, next) => {
+//    try {
+//        const {id} = req.params;
+//        await Recipe.findByIdAndDelete(id);
+//        return res.status(200).json('Recipe deleted!');
+//    } catch (error) {
+//        return next(error);
+//    }
+//});
+
+
+
+module.exports = router;
