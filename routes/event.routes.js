ecf5b3f8bff9a19ed19cf3fa7c436f76fac2b5c1
nicolascalise <nicolas.calise@bootcamp-upgrade.com>
2023-03-29 19:44:24 +0200

    refresh event route

--- routes/event.routes.js
+++ routes/event.routes.js
@@ -38,16 +38,20 @@ router.get('/:id', async (req, res, next) => {
 //Find by Name
 router.get('/name/:name', async (req, res, next) => {
 	const names = req.params.name;
-	try {
-		const event = await Event.find({name: names});
-		if (event) {
-			return res.status(200).json(event);
-		} else {
-			return res.status(404).json('No event found by this name');
-		}
-	} catch (error) {
-		return next(error);
-	}
+  if(names == ''){
+    console.log("No hay datos");
+  }else{
+	  try {
+	  	const event = await Event.find({name: names});
+	  	if (event) {
+	  		return res.status(200).json(event);
+	  	} else {
+	  		return res.status(404).json('No event found by this name');
+	  	}
+	  } catch (error) {
+	  	return next(error);
+	  }
+  }
 });
 
 router.post('/', async (req, res, next) => {
