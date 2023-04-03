b2c28012bf46dfd5f52e9368e642a3e8dab43220
DaniBootCamp <titodani@gmail.com>
2023-04-03 23:04:02 +0200

    added seed + photos

--- seeds/event.seed.js
+++ seeds/event.seed.js
@@ -10,7 +10,48 @@ const events = [
     date: '18-04-2023',
     eventType: ' Familiar', //not required
     capacity: 1000, //not required
-    duration:'2' //not required
+    duration:'2',
+    foto: "https://res.cloudinary.com/dgan7qtmg/image/upload/v1680555351/multer/circu.jpg" //not required
+  },
+  {
+    name: 'Rammstein',
+    description: 'Loocooo',
+    location: ' Galicia',
+    date: '12-06-2023',
+    eventType: ' Heavy', //not required
+    capacity: 1000, //not required
+    duration:'2', //not required
+    foto: "https://res.cloudinary.com/dgan7qtmg/image/upload/v1680555442/multer/rammstein.jpg"
+  },
+  {
+    name: 'Antonio Orozco',
+    description: 'Mi heroe',
+    location: ' Barcelona',
+    date: '21-06-2023',
+    eventType: ' Familiar', //not required
+    capacity: 1000, //not required
+    duration:'2',
+    foto: "https://res.cloudinary.com/dgan7qtmg/image/upload/v1680363023/qjjav27vudgjy86xgkge.jpg" //not required
+  },
+  {
+    name: 'Laura Paussini',
+    description: 'Ella',
+    location: ' Valencia',
+    date: '23-11-2023',
+    eventType: ' Familiar', //not required
+    capacity: 1000, //not required
+    duration:'2', //not required
+    foto: "https://res.cloudinary.com/dgan7qtmg/image/upload/v1680555600/multer/Laura.jpg"
+  },
+  {
+    name: 'Fito',
+    description: ' Y los fitipaldis',
+    location: ' Murcia',
+    date: '08-12-2023',
+    eventType: ' Familiar', //not required
+    capacity: 1000, //not required
+    duration:'2',
+    foto: "https://res.cloudinary.com/dgan7qtmg/image/upload/v1680555722/multer/Fito.jpg" //not required
   },
   {
   name: 'Iron Maiden',
@@ -19,7 +60,8 @@ const events = [
   date: '30-05-2023',
   eventType: ' Heavy Metal', //not required
   capacity: 500 , //not required
-  duration:'1:30' //not required
+  duration:'1:30', //not required
+  foto: "https://res.cloudinary.com/dgan7qtmg/image/upload/v1680555772/multer/Iron.jpg"
 }
 ];
 
